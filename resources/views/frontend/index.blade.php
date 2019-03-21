8b7870064358ab9b5aca6fe6f096b6ced5c6ba3a
Marcus <marcuschanjh@gmail.com>
2019-03-21 20:20:44 +0800

    Done and tested

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -8,38 +8,55 @@ $catCount = 0;
 $faqCount = 1;
 ?>
 
-    <div class="row">
-        @foreach( $category as $categoryValue )
-            @if(is_null($categoryValue->deleted_at))
+<div class="row">
+   
+    @foreach( $category as $categoryValue )
+        @if(is_null($categoryValue->deleted_at))
             <div class="col-xs-12">
-
                 <div class="panel panel-default">
-                    <div class="panel-heading"><h2>{{ $categoryValue->category }}</h2>  </div>
-                    @foreach($faqs as $faqValue)
-                        @if(is_null($faqValue->deleted_at) && $faqValue->status == 1)
-                            @if($faqValue->category == $categoryValue->category)
-                                <div class="panel-body">
-                                    <h3>{{$faqCount++}}.&nbsp; {{ $faqValue->question }}</h3>
-                                    {!!$faqValue->answer!!}
+                    <div class="panel-heading" role="tab" id="{{$catCount}}">
+                        <h4 class="panel-title">
+                            <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapse{{$catCount}}" aria-expanded="false" aria-controls="collapseThree">
+                                <h2>{{ $categoryValue->category }}</h2>
+                            </a>
+                        </h4>
+                    </div>
+
+                                <div id="collapse{{$catCount}}" class="panel-collapse collapse" role="tabpanel" aria-labelledby="{{$catCount}}">
+                                    @foreach($faqs as $faqValue)
+                                        @if(is_null($faqValue->deleted_at) && $faqValue->status == 1)
+                                            @if($faqValue->category == $categoryValue->category)
+                                    <div class="panel-body">
+                                        <h3>{{$faqCount++}}.&nbsp; {{ $faqValue->question }}</h3>
+                                        {!!$faqValue->answer!!}
+
+
+                                    </div>
+                                            @endif
+                                        @endif
+                                    @endforeach
                                 </div>
-                            @endif
-                        @endif
-                    @endforeach
+
+
                     <?php $faqCount = 1?>
-                </div><!-- panel -->
-            </div><!-- col-md-10 -->
-            @endif
-        @endforeach
-
-    </div>
-
+                    <?php $catCount++?>
+                </div>
+            </div>
+        @endif
+    @endforeach
 
 
 
 
+</div>
 
 
 
-        
+
+
+
+
+
+
 @endsection
 
